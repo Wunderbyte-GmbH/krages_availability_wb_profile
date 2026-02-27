c3ceafcbbc30cf7943a474359b2aa55b51bc29b8
David Bogner <davidbogner@gmail.com>
2026-02-27 16:54:05 +0100

    New version release

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024012900;
-$plugin->requires = 2020060900;
+$plugin->version = 2026022700;
+$plugin->requires = 2024100700;
 $plugin->component = 'availability_wb_profile';
